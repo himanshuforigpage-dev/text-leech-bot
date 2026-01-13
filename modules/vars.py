21318b419ce1588af6392885694bde380255d2b9
himanshuforigpage-dev <himanshuforigpage@gmail.com>
2026-01-13 18:06:41 +0530

    Set default API_ID, API_HASH, and BOT_TOKEN
    Updated API credentials with default values.

--- modules/vars.py
+++ modules/vars.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "21025319")
+API_HASH  = os.environ.get("API_HASH", "cfc533caac028e0e284dae2d571ad5b0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8457341970:AAEL7ez7cFFWDOx2n1BKiuYCUJwvibCK4U4") 
 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8870))  # Default to 8000 if not set
